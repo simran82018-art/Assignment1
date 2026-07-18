68dc047afef28e0778e34f37660038984c41f96a
simran82018-art <simran82018@gmail.com>
2026-07-18 18:10:14 +0530

    Complete q5 program

--- q5.java
+++ q5.java
@@ -3,5 +3,27 @@ public class q5 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int secret = 27;
+
+        for (int i = 1; i <= 5; i++) {
+
+            int guess = sc.nextInt();
+
+            if (guess == secret) {
+                System.out.println("Congratulations! You guessed it.");
+                break;
+            } else if (guess < secret) {
+                System.out.println("Too Low");
+            } else {
+                System.out.println("Too High");
+            }
+
+            if (i == 5) {
+                System.out.println("Better Luck Next Time!");
+            }
+        }
+
+        sc.close();
     
+}
 }
